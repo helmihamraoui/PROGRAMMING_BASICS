5740e0e7d74745fd110e2298d7e9030232bc2b39
helmihamraoui <helmihamraoui54@gmail.com>
2024-09-24 08:10:02 +0100

    creat variable

---
+++ helmi_hamraoui_JS_CreateVariables.js
@@ -0,0 +1,10 @@
+var min_age =10 ;
+var min_height=42;
+function valide (min_age , min_height){
+    if ( height < min_height && age < min_age) {
+        return "the conditions are not availabe";
+    }
+     else (height>= min_height && age >= min_age) 
+      {    return "the rider can go on the ride ";}
+    
+}
